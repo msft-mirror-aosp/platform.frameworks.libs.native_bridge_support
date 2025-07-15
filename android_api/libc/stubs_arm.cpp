590b1b4105ddbb2edb8787126b7b71f31816b0c6
Anthony Jones <anthonyjon@google.com>
2025-07-15 15:39:29 +0200

    Proxy strlen libc function
    ======================================================================
    Test                    ,    Before,     After,     Ratio,  Variance
    Single-Core
    File Compression        ,      4.32,      4.33,    1.0019,    0.0245
    Navigation              ,      2.34,      2.34,    1.0017,    0.0221
    HTML5 Browser           ,      0.54,      0.54,    0.9859,    0.0201
    PDF Renderer            ,      1.71,      1.71,    1.0035,    0.0152
    Photo Library           ,      4.25,      4.24,    0.9982,    0.0295
    Clang                   ,      2.30,      2.31,    1.0029,    0.0708
    Text Processing         ,      3.83,      3.82,    0.9980,    0.0369
    Asset Compression       ,      6.12,      6.12,    1.0013,    0.0066
    Object Detection        ,      3.39,      3.39,    1.0014,    0.0168
    Background Blur         ,      6.16,      6.17,    1.0017,    0.0153
    Horizon Detection       ,      2.03,      2.03,    0.9997,    0.0099
    Object Remover          ,      4.50,      4.53,    1.0064,    0.0110
    HDR                     ,      3.98,      3.98,    1.0001,    0.0127
    Photo Filter            ,      3.39,      3.40,    1.0013,    0.0218
    Ray Tracer              ,      1.98,      1.99,    1.0019,    0.0111
    Structure from Motion   ,      6.22,      6.24,    1.0036,    0.0058
    Geomean                 ,          ,          ,    1.0006,
    
    Flag: EXEMPT NDK
    Bug: 431768562
    Test: m berberis_all
    Change-Id: Iae8e3f31b40c5818b583653224efbf86b55a00fc

--- android_api/libc/stubs_arm.cpp
+++ android_api/libc/stubs_arm.cpp
@@ -114,6 +114,7 @@ DEFINE_INTERCEPTABLE_STUB_FUNCTION(pthread_sigqueue);
 DEFINE_INTERCEPTABLE_STUB_FUNCTION(setjmp);
 DEFINE_INTERCEPTABLE_STUB_FUNCTION(siglongjmp);
 DEFINE_INTERCEPTABLE_STUB_FUNCTION(sigsetjmp);
+DEFINE_INTERCEPTABLE_STUB_FUNCTION(strlen);
 DEFINE_INTERCEPTABLE_STUB_VARIABLE(environ);
 
 static void __attribute__((constructor(0))) init_stub_library() {
@@ -214,6 +215,7 @@ static void __attribute__((constructor(0))) init_stub_library() {
   INIT_INTERCEPTABLE_STUB_FUNCTION("libc.so", setjmp);
   INIT_INTERCEPTABLE_STUB_FUNCTION("libc.so", siglongjmp);
   INIT_INTERCEPTABLE_STUB_FUNCTION("libc.so", sigsetjmp);
+  INIT_INTERCEPTABLE_STUB_FUNCTION("libc.so", strlen);
   INIT_INTERCEPTABLE_STUB_VARIABLE("libc.so", environ);
 }
 // clang-format on
