b042a2418b807aa4ffc2e854f5845ffe69e7021b
Chaitanya Cheemala (xWF) <ccheemala@google.com>
2025-08-08 01:26:48 -0700

    Revert "Add libm proxy and stubs library"
    Revert submission 34783582-libm-native-bridge-support
    
    Reason for revert: Likely culprit for b/437273691 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Bug: 437273691
    
    Reverted changes: /q/submissionid:34783582-libm-native-bridge-support
    
    Change-Id: I8a24a60bbc7e1e453f993ec0708f9fcfb1aa8b97

--- android_api/libm/proxy/trampolines_riscv64_to_x86_64-inl.h
+++
@@ -1,8 +0,0 @@
-// clang-format off
-const KnownTrampoline kKnownTrampolines[] = {
-{"expf", GetTrampolineFunc<auto(float) -> float>(), reinterpret_cast<void*>(NULL)},
-{"powf", GetTrampolineFunc<auto(float, float) -> float>(), reinterpret_cast<void*>(NULL)},
-};  // kKnownTrampolines
-const KnownVariable kKnownVariables[] = {
-};  // kKnownVariables
-// clang-format on
--- native_bridge_support.mk
+++ native_bridge_support.mk
@@ -69,6 +69,7 @@ NATIVE_BRIDGE_ORIG_GUEST_LIBS := \
     libdl_android.bootstrap \
     libicu.bootstrap \
     liblog \
+    libm.bootstrap \
     libsqlite \
     libssl \
     libstdc++ \
@@ -92,7 +93,6 @@ NATIVE_BRIDGE_MODIFIED_GUEST_LIBS := \
     libGLESv2 \
     libGLESv3 \
     libjnigraphics \
-    libm \
     libmediandk \
     libnativehelper \
     libnativewindow \
